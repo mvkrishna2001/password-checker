71e08a7de71c4a9c6e7c49dc412280316c948470
babayaga <mvkrishna2001@gmail.com>
2021-09-23 17:31:01 +0530

    Suggesting a good check of all your passwords I am

--- main.py
+++ main.py
@@ -1,9 +1,52 @@
-# A super cool password checker
+import sys
 
-def print_hi(name):
-    print(f'Hi, {name}')
+from requests import get, Response
+from typing import List
+from hashlib import sha1
+
+
+def get_hashed_passwords(passwords: List[str]) -> List[str]:
+    ret = []
+    for password in passwords:
+        sha1pass = sha1(password.encode('utf-8')).hexdigest().upper()
+        ret.append(sha1pass)
+    return ret
+
+
+# we only give the first 5 characters of our sha1 password - k anonymity
+def request_matches(head: str) -> List[str]:
+    url = 'https://api.pwnedpasswords.com/range/' + '{}'.format(head)
+    res = get(url)
+
+    if res.status_code == 200:
+        return res.text.splitlines()
+    else:
+        raise RuntimeError(f'The status code is {res.status_code}')
+
+
+def check(tail: str, matches: List[str]) -> int:
+    ret = 0
+    for line in matches:
+        pas, count = line.split(':')
+        if tail == pas:
+           ret = count
+    return ret
+
+
+def respond(count: int) -> None:
+    if count == 0:
+        print("Your password has not been exposed. Pretty sure you are a hacker!")
+    else:
+        print(f"Might wanna change your password because it has been leaked {count} times")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    passwords = sys.argv[1:]
+    # passwords = ['hELlo']
+    hashs = get_hashed_passwords(passwords)
+    for password, hashed_password in zip(passwords, hashs):
+        head, tail = hashed_password[:5], hashed_password[5:]
+        matches = request_matches(head)
+        leak_count = check(tail, matches)
+        respond(leak_count)
 
