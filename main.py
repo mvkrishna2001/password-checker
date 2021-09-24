77ca07fde13bb51027d46fd36fc4ad874ba48971
babayaga <mvkrishna2001@gmail.com>
2021-09-24 14:02:46 +0530

    Almost ready.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-
-from requests import get, Response
+import pwinput
+from requests import get
 from typing import List
 from hashlib import sha1
+import time
 
 
 def get_hashed_passwords(passwords: List[str]) -> List[str]:
@@ -15,10 +15,12 @@ def get_hashed_passwords(passwords: List[str]) -> List[str]:
 
 # we only give the first 5 characters of our sha1 password - k anonymity
 def request_matches(head: str) -> List[str]:
+    # a nice website for checking this https://haveibeenpwned.com/passwords which also offers the api
     url = 'https://api.pwnedpasswords.com/range/' + '{}'.format(head)
     res = get(url)
 
     if res.status_code == 200:
+        # print(res.text.splitlines())
         return res.text.splitlines()
     else:
         raise RuntimeError(f'The status code is {res.status_code}')
@@ -41,12 +43,17 @@ def respond(count: int) -> None:
 
 
 if __name__ == '__main__':
-    passwords = sys.argv[1:]
+    # passwords = sys.argv[1:]
     # passwords = ['hELlo']
+    passwords = []
+    for idx in range(int(input("How many passwords do you wanna check: "))):
+        passwords.append(pwinput.pwinput(prompt="{}. Type your password here: ".format(idx+1), mask='*'))
+    start_time = time.time()
     hashs = get_hashed_passwords(passwords)
+
     for password, hashed_password in zip(passwords, hashs):
         head, tail = hashed_password[:5], hashed_password[5:]
         matches = request_matches(head)
         leak_count = check(tail, matches)
         respond(leak_count)
-
+        print(f'Checking this password took {time.time()-start_time} s')
